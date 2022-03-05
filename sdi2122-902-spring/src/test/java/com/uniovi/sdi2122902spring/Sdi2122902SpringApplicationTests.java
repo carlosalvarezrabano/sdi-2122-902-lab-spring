43e07d6f960b8029d5a4206bf6f3563921f81457
carlosalvarezrabano <carlos.arov@hotmail.com>
2022-03-05 12:58:46 +0100

    SDI-2122-902-5.1-Clase principal.

--- sdi2122-902-spring/src/test/java/com/uniovi/sdi2122902spring/Sdi2122902SpringApplicationTests.java
+++ sdi2122-902-spring/src/test/java/com/uniovi/sdi2122902spring/Sdi2122902SpringApplicationTests.java
@@ -1,10 +1,46 @@
 package com.uniovi.sdi2122902spring;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Sdi2122902SpringApplicationTests {
+
+    static String PathFirefox="C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    static String Geckodriver="C:\\Users\\carlo\\Downloads\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+
+    static WebDriver driver=getDriver(PathFirefox, Geckodriver);
+    static String URL="http://localhost:8090";
+
+    public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
+        System.setProperty("webdriver.firefox.bin", PathFirefox);
+        System.setProperty("webdriver.gecko.driver", Geckodriver);
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver.navigate().to(URL);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.manage().deleteAllCookies();
+    }
+
+    @BeforeAll
+    static public void begin() {
+
+    }
+
+    @AfterAll
+    static public void end() {
+        driver.quit();
+    }
 
     @Test
     void contextLoads() {
